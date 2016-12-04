1650b74cb0a27c5fc93ba48d382be62345e9ef06
muslikhamus45 <muslikhamus45@gmail.com>
2016-12-04 18:45:19 +0700

    Add files via upload

---
+++ sketch_aulia.uno.ino
@@ -0,0 +1,19 @@
+const int ledPin13 = 13; //lampu yang tersambung dipanel 12
+int analogValue = A0; // variable to hold the analog value
+void setup() {
+// open the serial port at 9600 bps:
+Serial.begin(9600);
+pinMode(ledPin13, OUTPUT);
+}
+void loop() {
+// read the analog input on pin 0:
+analogValue = analogRead(0);
+Serial.println(analogValue); // debug value
+if (analogValue >= 500) {
+digitalWrite(ledPin13, HIGH);
+}
+else if (analogValue < 100) {
+digitalWrite(ledPin13, LOW);
+}
+}
+
---
+++ sketch_musss.uno.ino
@@ -0,0 +1,16 @@
+int nilaiLDR = 0;            // variabel nilai awal untuk nilaiLDR
+void setup(){
+  pinMode(13, OUTPUT);          // menentukan LED pada pin 13 menjadi OUTPUT
+  Serial.begin(9600);          // komunikasi Arduino ke Komputer
+}
+void loop(){
+  nilaiLDR= analogRead(A2);      // menyimpan nilai yang dibaca dari LDR ke variabel nilaiLDR
+  Serial.print("NilaiLDR= ");   // menampilkan teks nilaiLDR=
+  Serial.println(nilaiLDR);    // menampilkan nilai dari nilaiLDR
+  if(nilaiLDR < 500) {        // jika nilai dari LDR kurang dari 500
+    digitalWrite(13, HIGH);  // lampu LED menyala
+  }
+  else  {                           
+    digitalWrite(13, LOW);  // lampu LED mati
+  }
+}
